15d3d8570a84baa177ff881ddbc34ac983329c69
Om8923 <142055626+Om8923@users.noreply.github.com>
2024-01-16 13:54:09 +0530

    Om changed

---
+++ ArrayD1.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+class ArrayD1 {
+    public static void main(String[] args) {
+        int arr[] = new int[] {5,6,2,3,1,9};
+        int N = 6;
+        for(int i=0;i<N;i++){
+            System.out.println(arr[i]);
+        }
+    }
+}
